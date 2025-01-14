142f1c6f527fc9b327426d11a19892e72cfe3369
Pham Nguyen Hung <pnhung1703@gmail.com>
2025-01-14 08:03:45 +0000

    Working on Chapter 2

---
+++ Chapter 2/vecAdd.cu
@@ -0,0 +1,21 @@
+void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
+    int size = n * sizeof(float);
+    float *A_d, *B_d, *C_d;
+
+    // Part 1: Move data from host to device
+    cudaMalloc((void **) &A_d, size);
+    cudaMalloc((void **) &B_d, size);
+    cudaMalloc((void **) &C_d, size);
+
+    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+    // Part 2: Kernel code
+
+    // Part 3: Copy result from device to host
+    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+}
