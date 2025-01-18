b01a244b76af570d60433a03717a88868a2e7593
HangenYuu <97870091+HangenYuu@users.noreply.github.com>
2025-01-18 15:22:53 +0800

    Finished code chapter 2

--- Chapter 2/vecAdd.cu
+++ Chapter 2/vecAdd.cu
@@ -1,17 +1,20 @@
-void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
+#include <math.h>
+
+void vecAdd(float *A_h, float *B_h, float *C_h, int n)
+{
     int size = n * sizeof(float);
     float *A_d, *B_d, *C_d;
 
     // Part 1: Move data from host to device
-    cudaMalloc((void **) &A_d, size);
-    cudaMalloc((void **) &B_d, size);
-    cudaMalloc((void **) &C_d, size);
+    cudaMalloc((void **)&A_d, size);
+    cudaMalloc((void **)&B_d, size);
+    cudaMalloc((void **)&C_d, size);
 
     cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
     cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
     // Part 2: Kernel code
-
+    vecAddKernel<<<ceil(n / 256.0), 256>>>(A_d, B_d, C_d, n);
     // Part 3: Copy result from device to host
     cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
 
